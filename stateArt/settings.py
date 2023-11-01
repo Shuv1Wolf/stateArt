281f9404f05830fcf8b2a29dd462eb909ac7156d
Shuv1Wolf <mskuratik06@gmail.com>
2023-11-02 00:07:58 +0300

    DUBUG = False

--- stateArt/settings.py
+++ stateArt/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -93,14 +93,20 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("POSTGRES_DB"),
-            'USER': os.getenv("POSTGRES_USER"),
-            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-            'HOST': os.getenv("POSTGRES_HOST"),
-            'PORT': os.getenv("POSTGRES_PORT")
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': os.getenv("POSTGRES_DB"),
+    #         'USER': os.getenv("POSTGRES_USER"),
+    #         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+    #         'HOST': os.getenv("POSTGRES_HOST"),
+    #         'PORT': os.getenv("POSTGRES_PORT")
+    #     }
+    # }
 
 
 # Password validation
