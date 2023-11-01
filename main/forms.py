ad0e29d7e323c6ee35aa62b22bfcc21a7b52b815
Shuv1Wolf <mskuratik06@gmail.com>
2023-11-01 22:08:35 +0300

    add 'ФОС', fancybox, bugfix

--- main/forms.py
+++ main/forms.py
@@ -3,34 +3,6 @@ from main.models import Article, News, Form
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV3, ReCaptchaV2Checkbox
 
-# class CreateArticleForm(forms.ModelForm):
-#     img_title = forms.ImageField(required=False)
-#     img_article = forms.ImageField(required=False)
-#
-#     class Meta:
-#         model = Article
-#         fields = [
-#             'title', 'img_title', 'slug', 'title_h1',
-#             'description', 'canonical', 'img_article'
-#         ]
-#
-#         field_args = {
-#             'img': {'required': False}
-#         }
-#
-#
-# class NewsForm(forms.ModelForm):
-#     img = forms.ImageField(required=True, blank=True, null=True)
-#
-#     class Meta:
-#         model = News
-#         fields = [
-#             'news', 'img'
-#         ]
-#
-#         field_args = {
-#             'img': {'required': True, 'blank': True, 'null': True}
-#         }
 
 
 class Form(forms.ModelForm):
